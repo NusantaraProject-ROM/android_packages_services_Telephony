51944b6a9400723bb54b1e85765ed7eb578929ce
Mengjun Leng <mengju@codeaurora.org>
2018-03-19 15:33:07 +0800

    TeleService: Add support for call timer reset on CDMA MO call
    In android original design, the duration of CDMA MO call is started from
    the dial command sent, so it is not the real duration of the active time.
    
    In this patch, a new message is registered to listen the event of the call
    accepted, and then reset the call duration when the event happens.
    
    Change-Id: Idae08bc17ad0044390ad8ebd34f7b4f29d35f7ed
    CRs-Fixed: 2207419

--- src/com/android/services/telephony/TelephonyConnection.java
+++ src/com/android/services/telephony/TelephonyConnection.java
@@ -1752,7 +1752,7 @@ abstract class TelephonyConnection extends Connection implements Holdable,
         setActive();
     }
 
-    private void close() {
+    protected void close() {
         Log.v(this, "close");
         clearOriginalConnection();
         destroy();
