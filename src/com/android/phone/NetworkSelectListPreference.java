ee6640020d3aaa14ccf55ec30353b7b1c3f4d46f
Ping Sun <ping.x.sun@sony.com>
2018-05-25 15:00:48 +0800

    Fix an issue wrong network operator name is displayed on MSIM devices
    SIM1 operator network name is displayed as SIM2 operator network name on
    Mobile Network setting because TelephonyManager#getNetworkOperatorName
    with no subId is always used to set the summary text.
    
    To resolve this issue, use TelephonyManager#getNetworkOperatorName with
    subId to get operator network name based on SIM.
    
    Bug: 109936826
    Test: manual - Checked that the network name is displayed according to
    the SIM network operator
    Change-Id: Ief6d5148ef0e27bb31d869994cf64ba26ce4f7d4
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/phone/NetworkSelectListPreference.java
+++ src/com/android/phone/NetworkSelectListPreference.java
@@ -210,7 +210,7 @@ public class NetworkSelectListPreference extends ListPreference
         mNetworkScanHelper = new NetworkScanHelper(
                 mTelephonyManager, mCallback, mNetworkScanExecutor);
 
-        setSummary(mTelephonyManager.getNetworkOperatorName());
+        setSummary(mTelephonyManager.getNetworkOperatorName(mSubId));
 
         setOnPreferenceChangeListener(this);
     }
