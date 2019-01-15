217875cb4107d2ad4e04065e46a4c370c4ac8968
qiongz <qiongz@codeaurora.org>
2019-01-15 10:10:11 +0800

    Include sub id extra even when sim is locked.
    The origin design only can get sim app state for default sub
    id. It is not adapt to multi sim.
    Remove the judgment of sim application state to include sub
    id extra even when sim is locked since sub ids are available.
    
    Change-Id: I508b4513143905775b70eacf7b211632830c7a9f
    CRs-Fixed: 2352445

--- src/com/android/phone/CarrierConfigLoader.java
+++ src/com/android/phone/CarrierConfigLoader.java
@@ -526,11 +526,8 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
         Intent intent = new Intent(CarrierConfigManager.ACTION_CARRIER_CONFIG_CHANGED);
         intent.addFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY_BEFORE_BOOT |
                 Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND);
-        // Include subId/carrier id extra only if SIM records are loaded
-        TelephonyManager telephonyManager = TelephonyManager.from(mContext);
-        int simApplicationState = telephonyManager.getSimApplicationState();
-        if (addSubIdExtra && (simApplicationState != TelephonyManager.SIM_STATE_UNKNOWN
-                && simApplicationState != TelephonyManager.SIM_STATE_NOT_READY)) {
+        // Include subId extra only if SIM records are loaded
+        if (addSubIdExtra) {
             SubscriptionManager.putPhoneIdAndSubIdExtra(intent, phoneId);
             intent.putExtra(TelephonyManager.EXTRA_PRECISE_CARRIER_ID,
                     getPreciseCarrierIdForPhoneId(phoneId));
